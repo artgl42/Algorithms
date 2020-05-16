f1a22586dfd593bb5197a9acbe16c1b9f7652759
artgl42 <artgl.net@gmail.com>
2020-05-16 21:27:47 +0700

    Add ShellSort

---
+++ SortingLibrary/SortingLibrary/SortingMethod/ShellSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SortingLibrary
+{
+    public class ShellSort : ISortingMethod
+    {
+        public ulong Sort<T>(T[] arrayForSort) where T : IComparable<T>
+        {
+            T _key;
+            ulong _count = 0;
+            int _indexBackValue, _indexFrontValue;
+            int _delta = arrayForSort.Length >> 1;
+
+            while (_delta > 0)
+            {
+                _indexFrontValue = _delta;
+                while (_indexFrontValue < arrayForSort.Length)
+                {
+                    _key = arrayForSort[_indexFrontValue];
+                    _indexBackValue = _indexFrontValue - _delta;
+                    while (_indexBackValue >= 0 && arrayForSort[_indexBackValue].CompareTo(_key) > 0)
+                    {
+                        arrayForSort[_indexBackValue + _delta] = arrayForSort[_indexBackValue];
+                        _indexBackValue -= _delta;
+                        _count++;
+                    }
+                    arrayForSort[_indexBackValue + _delta] = _key;
+                    _indexFrontValue++;
+                }
+                _delta >>= 1;
+            }
+
+            return _count;
+        }
+    }
+}
